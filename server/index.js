7a63c1f7cd58c9de76bdc0e25e42d83a3db49530
Janujah <janujahsivarattinam@gmail.com>
2024-05-31 23:01:37 +0530

    Deploy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 app.use(
     cors({
-      origin: ["http://localhost:3000","https://psd-1.onrender.com"],
+      origin: ["https://oasis-f-inal-directory-6cg7.vercel.app"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })

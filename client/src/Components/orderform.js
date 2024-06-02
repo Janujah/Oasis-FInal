45acfe549c69f5bb9cef3630e28a2367faaf42b3
Janujah <janujahsivarattinam@gmail.com>
2024-06-02 16:38:32 +0530

    Deploy

--- client/src/Components/orderform.js
+++ client/src/Components/orderform.js
@@ -1,105 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode';
-import { loadStripe } from '@stripe/stripe-js';
 import Nav from '../Components/navbar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import StripeCheckout from 'react-stripe-checkout';
-
-const stripePromise = loadStripe('pk_test_51PGywdBwqNocB2yIgRUhTOxCLUXifycb47usxmBwsizanJt9hIuwOLGasezA5xeXFukFgxT4UPmcqTRPl8ekCh3M00GL7hsLNh');
 
 const OrderForm = () => {
   const { productId } = useParams();
   const location = useLocation();
-  const navigate = useNavigate();
   const [productName, setProductName] = useState(location.state?.productName || '');
   const [customerName, setCustomerName] = useState('');
   const [userId, setUserId] = useState('');
   const [address, setAddress] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
-  const [orderPlaced, setOrderPlaced] = useState(false);
-  const [orderData, setOrderData] = useState(null);
-  const [booking, setBooking] = useState({
-    name: "Order the Product",
-    price: 1500 
-  });
-  const [paymentProcessed, setPaymentProcessed] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem('auth-token');
     if (token) {
       const decodedToken = jwtDecode(token);
-      setCustomerName(decodedToken.userName); // Adjust based on your token structure
-      setUserId(decodedToken.id); // Adjust based on your token structure
+      setCustomerName(decodedToken.userName);
+      setUserId(decodedToken.id); 
     }
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://oasis-final-directory.onrender.com/order', {
+      const response = await fetch('http://localhost:3002/order', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           productName,
+          userId,
           customerName,
           address,
           phoneNumber,
         }),
       });
-
-      if (!response.ok) {
-        throw new Error('Failed to place order.');
+      if (response.ok) {
+        toast.success('Order placed successfully!', { position: 'bottom-right' });
+      } else {
+        toast.error('Failed to place order.', { position: 'bottom-right' });
       }
-
-      const data = await response.json();
-      toast.success('Order placed successfully! Please proceed with the payment.', { position: 'bottom-right' });
-      setOrderData(data);
-      setOrderPlaced(true);
-      setPaymentProcessed(true); // Allow payment processing
     } catch (error) {
       console.error('Error placing order:', error);
       toast.error('Failed to place order.', { position: 'bottom-right' });
-    }
-  };
-
-  const handleToken = async (token) => {
-    try {
-      const response = await fetch('https://oasis-final-directory.onrender.com/booking/payment', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token, orderData }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Payment failed');
-      }
-
-      const data = await response.json();
-      toast.success('Payment Successful!', { position: 'bottom-right' });
-
-      const addToDashboardResponse = await fetch('https://oasis-final-directory.onrender.com/booking/payment/success', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ orderId: data.orderId, userId: data.userId }),
-      });
-
-      if (!addToDashboardResponse.ok) {
-        throw new Error('Failed to add order to dashboard');
-      }
-
-      toast.success('Order added to your dashboard!', { position: 'bottom-right' });
-      navigate('/');
-    } catch (error) {
-      toast.error('Payment failed. Please try again.', { position: 'bottom-right' });
-      console.error('Payment error:', error);
     }
   };
 
@@ -119,6 +66,7 @@ const OrderForm = () => {
                 onChange={(e) => setCustomerName(e.target.value)}
                 required
                 style={{ padding: '10px', width: '100%', marginTop: '5px' }}
+                disabled
               />
             </div>
             <div style={{ marginBottom: '10px' }}>
@@ -146,20 +94,11 @@ const OrderForm = () => {
           <ToastContainer />
         </div>
       </div>
-      {paymentProcessed && (
-        <div className="container" style={{ textAlign: 'center', marginTop: '20px' }}>
-          <StripeCheckout
-            stripeKey="pk_test_51PGywdBwqNocB2yIgRUhTOxCLUXifycb47usxmBwsizanJt9hIuwOLGasezA5xeXFukFgxT4UPmcqTRPl8ekCh3M00GL7hsLNh"
-            token={handleToken}
-            name="Order Payment"
-            amount={booking.price * 100} 
-          >
-            <button className="btn btn-success mt-4">Confirm Payment</button>
-          </StripeCheckout>
-        </div>
-      )}
     </div>
   );
 };
 
 export default OrderForm;
+
+
+

d2ee26579e5882b218be8e23bac25d18e214da20
Janujah <janujahsivarattinam@gmail.com>
2024-06-11 22:49:53 +0530

    Login-Auth

--- server/Routes/loginRoutes.js
+++ server/Routes/loginRoutes.js
@@ -3,10 +3,11 @@
 const express = require("express");
 const router = express.Router();
 const LogInControl = require("../Controller/logincontroller");
+const checkAuthenticated = require('../utils/loginAuth');
 
 //routes using controller function
 
-router.post("/login", LogInControl.loginUser);
+router.post("/login", checkAuthenticated, LogInControl.loginUser);
 
 
 module.exports = router;

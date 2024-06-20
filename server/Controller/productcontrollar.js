0b359098f6c7ad2c65dd5382d478f4cd063214d7
Janujah <janujahsivarattinam@gmail.com>
2024-06-20 20:24:32 +0530

    Login-Auth

--- server/Controller/productcontrollar.js
+++ server/Controller/productcontrollar.js
@@ -62,3 +62,20 @@ exports.deleteUser = async (req, res) => {
       res.status(500).json({ message: 'Error deleting doctor', error });
   }
 };
+
+
+exports.getProductById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (error) {
+    console.error('Error fetching product:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
